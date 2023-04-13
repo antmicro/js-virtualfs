170564174dca27f159d3df797e346b0c14459bbf
Grzegorz Placzek <gplaczek@antmicro.com>
2023-04-13 14:24:23 +0200

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -11,84 +11,8 @@ export default [
   {
     input: 'lib/index.js',
     output: {
-      file: 'dist/index.node.es.js',
-      format: 'es'
-    },
-    external: (id) => {
-      return Object.keys(packageJson.dependencies)
-        .concat(Object.keys(packageJson.devDependencies))
-        .map((dep) => new RegExp('^' + dep))
-        .concat([
-            /^babel-runtime/,
-            /^buffer/,
-            /^events/,
-            /^process/,
-            /^path/,
-            /^stream/
-        ])
-        .some((pattern) => pattern.test(id));
-    },
-    plugins: [
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        runtimeHelpers: true,
-        plugins: ['transform-object-rest-spread', 'transform-runtime'],
-        presets: [
-          'flow',
-          ['env', {
-            modules: false,
-            targets: {
-              node: '6.4.0'
-            }
-          }]
-        ]
-      })
-    ]
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.node.cjs.js',
-      format: 'cjs'
-    },
-    external: (id) => {
-      return Object.keys(packageJson.dependencies)
-        .concat(Object.keys(packageJson.devDependencies))
-        .map((dep) => new RegExp('^' + dep))
-        .concat([
-            /^babel-runtime/,
-            /^buffer/,
-            /^events/,
-            /^process/,
-            /^path/,
-            /^stream/
-        ])
-        .some((pattern) => pattern.test(id));
-    },
-    plugins: [
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        runtimeHelpers: true,
-        plugins: ['transform-object-rest-spread', 'transform-runtime'],
-        presets: [
-          'flow',
-          ['env', {
-            modules: false,
-            targets: {
-              node: '6.4.0'
-            }
-          }]
-        ]
-      })
-    ]
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.browser.umd.js',
-      format: 'umd',
+      file: 'dist/vfs.js',
+      format: 'es',
       name: 'virtualfs'
     },
     plugins: [
